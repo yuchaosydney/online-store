8fe3a8e8053ac4ed786e85d97399e45875803357
yuchaosydney <yuchao.sydneyuni@gmail.com>
2018-09-06 09:34:42 +1000

    in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler, APP_INITIALIZER } from '@angular/core';
-import { HttpModule, RequestOptions } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
@@ -18,20 +18,9 @@ import { AppConfigService } from './services';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 
-import { StoreModule, MetaReducer } from '@ngrx/store';
+import { MetaReducer } from '@ngrx/store';
 import { storeFreeze } from 'ngrx-store-freeze';
-import { ProductEffects } from './store/effects/product.effects';
-import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FileDropZoneComponent } from './components/file-drop-zone/file-drop-zone.component';
-import { DeleteProductConfirmDialogComponent } from './components/delete-product-confirm-dialog/delete-product-confirm-dialog.component';
-import { ProductFormComponent } from './components/product-form/product-form.component';
-
-import { reducers } from './store';
-
-// guards
-import * as fromGuards from './guards';
 
 // this would be done dynamically with webpack for builds
 const environment = {
@@ -50,8 +39,7 @@ export const ROUTES: Routes = [
   },
   {
     'path': 'dashboard',
-    component: DashboardComponent,
-    canActivate: [fromGuards.AuthGuard, fromGuards.ProductsGuard]
+    loadChildren: '../dashboard/dashboard.module#DashboardModule'
   }
 ];
 
@@ -64,13 +52,9 @@ const appInitializerFn = (appConfig: AppConfigService) => {
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
-    ProductFormComponent,
-    DeleteProductConfirmDialogComponent,
-    DashboardComponent,
-    FileDropZoneComponent
+    AuthComponent
   ],
-  entryComponents: [ProductFormComponent, DeleteProductConfirmDialogComponent],
+  entryComponents: [],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
@@ -79,10 +63,6 @@ const appInitializerFn = (appConfig: AppConfigService) => {
     RouterModule.forRoot(ROUTES),
     ModalModule.forRoot(),
     ProgressbarModule.forRoot(),
-    StoreModule.forRoot(reducers, { metaReducers }),
-    StoreModule.forFeature('products', reducers),
-    EffectsModule.forFeature([ProductEffects]),
-    EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument()
   ],
   providers: [
@@ -100,8 +80,7 @@ const appInitializerFn = (appConfig: AppConfigService) => {
     {
       provide: ErrorHandler,
       useClass: AuthErrorHandler
-    },
-    fromGuards.guards
+    }
   ],
   bootstrap: [AppComponent]
 })
